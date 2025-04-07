26faf8aebed7feb0e2bf9aae12c47dce4619c98f
nisel <nisel11good@gmail.com>
2025-04-07 12:08:46 +0300

    Initial commit
    pacman plugin from https://github.com/axtloss/vib-plugins/tree/main/pacman
    workflow from https://github.com/Vanilla-OS/vib-plugin/blob/main/.github/workflows/build.yml

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/axtloss/vib-pacman
+
+go 1.21
+
+require github.com/mitchellh/mapstructure v1.5.0
+
+require github.com/vanilla-os/vib/api v0.0.0-20250109193218-3a71c668ee26 // indirect
---
+++ plugin.go
@@ -0,0 +1,52 @@
+// Copyright 2023 - 2023, axtlos <axtlos@disroot.org>
+// SPDX-License-Identifier: GPL-3.0-ONLY
+
+package main
+
+import (
+	"C"
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+type PacmanModule struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+
+	ExtraFlags []string
+	Packages   []string
+}
+
+//export PlugInfo
+func PlugInfo() *C.char {
+	plugininfo := &api.PluginInfo{Name: "pacman", Type: api.BuildPlugin, UseContainerCmds: false}
+	pluginjson, err := json.Marshal(plugininfo)
+	if err != nil {
+		return C.CString(fmt.Sprintf("ERROR: %s", err.Error()))
+	}
+	return C.CString(string(pluginjson))
+}
+
+func convertToCString(s string) *C.char {
+	return C.CString(s)
+}
+
+func convertToGoString(s *C.char) string {
+	return C.GoString(s)
+}
+
+//export BuildModule
+func BuildModule(moduleInterface *C.char, _ *C.char) *C.char {
+	var module PacmanModule
+	err := json.Unmarshal([]byte(C.GoString(moduleInterface)), &module)
+	if err != nil {
+		return C.CString(fmt.Sprintf("ERROR: %s", err.Error()))
+	}
+
+	cmd := fmt.Sprintf("pacman -S --noconfirm %s %s", strings.Join(module.ExtraFlags, " "), strings.Join(module.Packages, " "))
+
+	return C.CString(cmd)
+}
+
+func main() {}
